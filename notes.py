2905333ac8ee5ff4a8114d1c710ef915d2d0541d
joetechem <joe@techemstudios.com>
2017-09-20 01:13:28 -0400

    notes

---
+++ notes.py
@@ -0,0 +1,10 @@
+"""
+After Class Notes
+"""
+
+
+# 9/19
+	# skipped expressions & statements
+	# ended with variables (msg.py)
+		# Might be best to stay in terminal together for longer (Quicker responses)
+		# Move from terminal when functions/oop start
