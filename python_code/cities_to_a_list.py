23b110b640666722256f3971166cc9a48f56e3af
joetechem <joe@techemstudios.com>
2017-09-26 16:06:02 -0400

    latest deck

--- python_code/cities_to_a_list.py
+++ python_code/cities_to_a_list.py
@@ -2,18 +2,23 @@
 A simple program that takes the user's input, and adds it to a list.
 """
 
+
 cities = []
+
 def main():
-    def instructions():
-        print("\n\tType in any city name I should visit, and I'll add it to a list.")
-        print("\tTo quit, type <done>")
-    instructions()
-
-
+#    def instructions():
+#        print("\n\tType in any city name I should visit, and I'll add it to a list.")
+#        print("\tTo quit, type <done>")
+#    instructions()
+    prompt = "\n\tType in any city name I should visit, and I'll add it to a list."
+    prompt += "\n\tTo quit, type <done>"
+    prompt += "\n\nCity name: "
+    
     active = True
     while active:
-        new_city = raw_input("\nEnter a city name: ")
+        new_city = raw_input(prompt)
         cities.append(new_city)
+        print("\nI'd love to go " + new_city.title() + "!")
 
         if new_city == 'done':
             print("Here is the list of cities so far:")
