515bb02901cedfdd06d97022473a5ad481e59103
joetechem <joe@techemstudios.com>
2017-09-26 11:57:17 -0400

    updating random game

---
+++ python_code/random_number_game.py
@@ -0,0 +1,23 @@
+import random
+
+number = random.randint(1, 100)
+print("I'm thinking of a number between 1 and 100.")
+GameRunning = True
+
+while True:
+    guess = int(raw_input("Guess a number between 1-100: "))
+
+    if guess == number:
+        GameRunning = True
+        print("You guessed correctly!")
+        
+    elif guess > number:
+        GameRunning = True
+        print("Your guess was too high!")
+        
+    elif guess < number:
+        GameRunning = True
+        print("Your guess was too low!")
+
+# Need to change so that another random number is picked, while the user
+# is still in the loop after guessing correctly.
