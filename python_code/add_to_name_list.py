aabb98c52826fb768494d8aab5b569d646c6cfbe
joetechem <joe@techemstudios.com>
2017-09-26 15:04:27 -0400

    combining concepts challenge

--- python_code/add_to_name_list.py
+++ python_code/add_to_name_list.py
@@ -5,14 +5,18 @@ names = []
 # Set new_name to something other than 'quit'.
 new_name = ''
 
-# Start a loop that will run until the user enters 'quit'.
-while new_name != 'quit':
-    # Ask the user for a name.
-    new_name = raw_input("Please tell me someone I should know, or enter 'quit': ")
 
-    # Add the new name to our list.
-    if new_name != 'quit':
-        names.append(new_name)
+main():
+    # Start a loop that will run until the user enters 'quit'.
+    while new_name != 'quit':
+        # Ask the user for a name.
+        new_name = raw_input("Please tell me someone I should know, type <quit> to show the list of names so far: ")
 
-# Show that the name has been added to the list.
-print(names)
+        # Add the new name to our list.
+        if new_name != 'quit':
+            names.append(new_name)
+
+    # Show that the name has been added to the list.
+    print(names)
+    
+main()
