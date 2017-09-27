7273d6e6260295fcd5e63d6a5261985c20a12727
joe <joe@techemstudios.com>
2017-09-27 15:36:09 -0400

    Update notes.py

--- notes.py
+++ notes.py
@@ -8,3 +8,10 @@ After Class Notes
 	# ended with variables (msg.py)
 		# Might be best to stay in terminal together for longer (Quicker responses)
 		# Move from terminal when functions/oop start
+		
+# 9/26
+	# Took way to long to configure GitHub accts
+		# Next time, send instructions to do at home
+	# went over expressions, statments, variable and string concatenation
+	# one successful git push!
+	# start on user input next week
