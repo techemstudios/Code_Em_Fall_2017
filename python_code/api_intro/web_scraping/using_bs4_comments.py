e3c40055209ccbd8a246dd142f9d334b5dfc30bf
joetechem <joe@techemstudios.com>
2017-11-14 17:00:33 -0500

    descriptive comments

--- python_code/api_intro/web_scraping/using_bs4_comments.py
+++ python_code/api_intro/web_scraping/using_bs4_comments.py
@@ -1,33 +1,30 @@
-"""
-Web scrape to get next week's recipe titles and descriptions from Blue Apron.
-
-Example of retrieving data from a site, with no need of api key.
-
-Pull html, parse it, handle it to return values that make sense.
-"""
-
 from urllib2 import urlopen
 import requests
 from bs4 import BeautifulSoup as soup
 
 url = 'https://www.blueapron.com/pages/sample-recipes'
 
+# open connection, grab the page (offloading content into variable)
 uClient = urlopen(url)
 page_html = uClient.read()
 uClient.close()
 
+# html parsing
 page_soup = soup(page_html, "html.parser")
 
+# grabs each product
 recipes = page_soup.findAll("div",{"class":"tab-content js-initialMenu"})
 
+# loop through list to narrow down results
+# and change encoding to omit u' tags
 for recipe in recipes:
     title = recipe.findAll("div", {"class":"recipe-title"}) #**
     title[0].encode("ascii")
 
+# loop through narrowed down list to return items without html tags
 for name in title:
     titles = name.text
-    print(titles)
+    print titles
 
 
-
-
+    
