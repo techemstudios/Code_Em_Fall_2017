9a1980a2a29f47ae8d8bc15dd4d1fcf996f4574e
joetechem <joe@techemstudios.com>
2017-11-29 13:51:32 -0500

    turkey update

--- python_code/object-oriented/objects_and_graphics/shapes.py
+++ python_code/object-oriented/objects_and_graphics/shapes.py
@@ -16,19 +16,19 @@ circ = Circle(center, 90)
 circ.setFill('red')
 circ.draw(win)
 
-### Put a textual label in center of circle
-label = Text(center, "Red Circle")
-label.draw(win)
-
-### Draw a square using Rectangle object
-rect = Rectangle(Point(90, 90), Point(210, 210))
-rect.draw(win)
-
-### Draw a line segment using a Line object
-line = Line(Point(60, 90), Point(540, 495))
-line.draw(win)
-
-### Draw an oval using the Oval object
-oval = Oval(Point(60, 450), Point(540, 597))
-oval.draw(win)
-win.getMouse()
+##### Put a textual label in center of circle
+##label = Text(center, "Red Circle")
+##label.draw(win)
+##
+##### Draw a square using Rectangle object
+##rect = Rectangle(Point(90, 90), Point(210, 210))
+##rect.draw(win)
+##
+##### Draw a line segment using a Line object
+##line = Line(Point(60, 90), Point(540, 495))
+##line.draw(win)
+##
+##### Draw an oval using the Oval object
+##oval = Oval(Point(60, 450), Point(540, 597))
+##oval.draw(win)
+##win.getMouse()
